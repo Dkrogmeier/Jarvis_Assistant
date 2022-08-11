2daf1c30d03c76dd08bc8e0167889d6fb0c5dadd
Dakota Krogmeier <dakotakrogmeier@gmail.com>
2022-08-11 13:24:35 -0500

    Finished Article, added remaining functions and modules. JARVIS is alive, but is missing some keys and isnt a great listener. Bug #1

--- functions/online_ops.py
+++ functions/online_ops.py
@@ -3,12 +3,15 @@ from shutil import ExecError
 import requests
 import wikipedia
 import pywhatkit as kit
-import email.message import EmailMessage
+from email.message import EmailMessage
 import smtplib
 from decouple import config
 
 EMAIL = config('EMAIL')
 PASSWORD = config('PASSWORD')
+NEWS_API_KEY = config('NEWS_API_KEY')
+OPENWEATHER_APP_ID = config('OPENWEATHER_APP_ID')
+TMDB_API_KEY = config("TMDB_API_KEY")
 
 
 def find_my_ip():
@@ -45,3 +48,45 @@ def send_email(receiver_address, subject, message):
     except Exception as e:
         print(e)
         return False
+
+
+def get_latest_news():
+    news_headlines = []
+    res = requests.get(
+        f"https://newsapi.org/v2/top-headlines?country=us&apiKey={NEWS_API_KEY}&category=general").json()
+    articles = res["articles"]
+    for article in articles:
+        news_headlines.append(article["title"])
+    return news_headlines[:5]
+
+
+def get_weather_report(city):
+    res = requests.get(
+        f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPENWEATHER_APP_ID}&units=metric").json()
+    weather = res["weather"][0]["main"]
+    temperature = res["main"]["temp"]
+    feels_like = res["main"]["feels_like"]
+    return weather, f"{temperature} C", f"{feels_like} C"
+
+
+def get_trending_movies():
+    trending_movies = []
+    res = requests.get(
+        f"https://api.themoviedb.org/3/trending/movie/day?api_key={TMDB_API_KEY}").json()
+    results = res["results"]
+    for r in results:
+        trending_movies.append(r["original_title"])
+    return trending_movies[:5]
+
+
+def get_random_joke():
+    headers = {
+        'Accept': 'application/json'
+    }
+    res = requests.get("https://icanhazdadjoke.com/", headers=headers).json()
+    return res["joke"]
+
+
+def get_random_advice():
+    res = requests.get("https://api.adviceslip.com/advice").json()
+    return res['slip']['advice']
